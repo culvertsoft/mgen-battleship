6f0a8780d039b7ccbfa91a0dd7468137fc8fcaa9
Johan Kjölhede <kjolhede@telia.com>
2014-10-04 01:02:51 +0200

    Adding some more logic

--- server/src/main/java/battleship/server/Server.java
+++ server/src/main/java/battleship/server/Server.java
@@ -15,7 +15,24 @@ import battleship.messages.ShipPlacement;
 import battleship.messages.TeamSelect;
 import battleship.state.Player;
 
-public class Server implements NetworkListener {
+public class Server {
+
+	/**
+	 * Creates a new server without adding any backends. Use addBackend(..) to
+	 * add backends and then call init().
+	 */
+	public Server() {
+	}
+
+	/**
+	 * Adds a listening backend to this Server. Must be called before init
+	 * 
+	 * @param backend
+	 *            The backend to add
+	 */
+	public void addBackend(final BackEnd backend) {
+		m_backEnds.add(backend);
+	}
 
 	/**
 	 * Starts this clients internal threads.
@@ -35,40 +52,19 @@ public class Server implements NetworkListener {
 		}
 	}
 
-	@Override
-	public void onConnect(final Client client) {
-		System.out.println("client connected, waiting for login from: " + client);
-	}
-
-	@Override
-	public void onDisconnect(final Client client, final String reason) {
-		if (m_players.containsKey(client)) {
-			m_currentClient = client;
-			m_currentPlayer = m_players.get(client);
-			m_gameListener.onLeft(m_players.get(client), reason);
-		}
-	}
-
-	@Override
-	public void onMessage(final Client client, final Message message) {
-		m_currentClient = client;
-		m_currentPlayer = m_players.get(client);
-		Dispatcher.dispatch(message, m_gameListener);
-	}
-
-	@Override
-	public void onError(final Exception error, final Object source) {
-		System.err.println("Error from: " + source);
-		error.printStackTrace();
+	/**
+	 * Gets the internal NetworkListener interface. This is attachable to new
+	 * BackEnds that may be added to this Server.
+	 * 
+	 * @return The internal NetworkListener interface of this Server.
+	 */
+	public NetworkListener networkListener() {
+		return m_networkListener;
 	}
 
 	// ////////////////////////////////////////////////////////////// //
-	// /////////////////////// PRIVATE ////////////////////////////// //
+	// ////////////////////// GAME LOGIC //////////////////////////// //
 	// ////////////////////////////////////////////////////////////// //
-
-	private void reply(final Message message) {
-		m_currentClient.send(message);
-	}
 
 	private final GameServerListener m_gameListener = new GameServerListener() {
 
@@ -119,6 +115,48 @@ public class Server implements NetworkListener {
 		}
 
 	};
+
+	// ////////////////////////////////////////////////////////////// //
+	// ////////////////////// NETWORK LOGIC ///////////////////////// //
+	// ////////////////////////////////////////////////////////////// //
+
+	private final NetworkListener m_networkListener = new NetworkListener() {
+
+		@Override
+		public void onConnect(final Client client) {
+			System.out.println("client connected, waiting for login from: " + client);
+		}
+
+		@Override
+		public void onDisconnect(final Client client, final String reason) {
+			if (m_players.containsKey(client)) {
+				m_currentClient = client;
+				m_currentPlayer = m_players.get(client);
+				m_gameListener.onLeft(m_players.get(client), reason);
+			}
+		}
+
+		@Override
+		public void onMessage(final Client client, final Message message) {
+			m_currentClient = client;
+			m_currentPlayer = m_players.get(client);
+			Dispatcher.dispatch(message, m_gameListener);
+		}
+
+		@Override
+		public void onError(final Exception error, final Object source) {
+			System.err.println("Error from: " + source);
+			error.printStackTrace();
+		}
+	};
+
+	// ////////////////////////////////////////////////////////////// //
+	// ////////////////////// MISC PRIVATE ////////////////////////// //
+	// ////////////////////////////////////////////////////////////// //
+
+	private void reply(final Message message) {
+		m_currentClient.send(message);
+	}
 
 	private Client m_currentClient = null;
 	private Player m_currentPlayer = null;
