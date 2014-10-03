4c48bd822658093baef621fa35c20cab514faa47
Johan Kjölhede <kjolhede@telia.com>
2014-10-03 16:02:49 +0200

    fixedd connected flag

--- client-java/src/main/java/battleship/network/GameClient.java
+++ client-java/src/main/java/battleship/network/GameClient.java
@@ -79,11 +79,13 @@ public class GameClient {
 
 		@Override
 		public void onDisconnect() {
+			m_connected = false;
 			m_listener.onDisconnect();
 		}
 
 		@Override
 		public void onConnect() {
+			m_connected = true;
 			m_network.sendMessage(new Login(m_nickName));
 			m_listener.onConnect();
 		}
